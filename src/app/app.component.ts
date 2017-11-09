3001e663f553fcdd33287a2cf08a3f8f0dd536ba
António Pinho <antonio.henrique@ua.pt>
2017-11-09 21:40:16 +0000

    09-11-2017

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Primeiro';
+  inativo: boolean = true;
+
+  texto: string = 'Texto do placeholder';
+
+  constructor(){
+    this.colocarAtivo();
+  }
+
+  colocarAtivo(){
+    setTimeout(() => {
+      this.inativo = false;
+      this.texto = 'Texto alterado em 5s';
+    }, 5000);
+  }
+
+
 }
